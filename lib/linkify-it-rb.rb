f92907cc0f250d11fc57562a1f0d2eed5cf86143
digitalMoksha <brett@digitalmoksha.com>
2018-04-02 15:59:42 +0200

    make sure re.rb is compiled before index.rb for RubyMotion
    make sure re.rb is compiled before index.rb for RubyMotion

--- lib/linkify-it-rb.rb
+++ lib/linkify-it-rb.rb
@@ -1,10 +1,10 @@
-# encoding: utf-8
-
 if defined?(Motion::Project::Config)
 
   lib_dir_path = File.dirname(File.expand_path(__FILE__))
   Motion::Project::App.setup do |app|
-    app.files.unshift(Dir.glob(File.join(lib_dir_path, "linkify-it-rb/**/*.rb")))
+    app.files.unshift(Dir.glob(File.join(lib_dir_path, 'linkify-it-rb/**/*.rb')))
+
+    app.files_dependencies File.join(lib_dir_path, 'linkify-it-rb/index.rb') => File.join(lib_dir_path, 'linkify-it-rb/re.rb')
   end
 
   require 'uc.micro-rb'
@@ -12,7 +12,7 @@ if defined?(Motion::Project::Config)
 else
 
   require 'uc.micro-rb'
-  require 'linkify-it-rb/linkify_re'
+  require 'linkify-it-rb/re'
   require 'linkify-it-rb/index'
 
 end
